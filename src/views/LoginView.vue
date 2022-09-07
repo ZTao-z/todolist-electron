86e4f01fa533d0ce5bf82793a7f67cc8018236d3
qingyunhuohuo <948712633@qq.com>
2022-09-08 00:27:57 +0800

    [Opt] 跑通protobuf

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,25 +3,13 @@
     <v-container>
       <v-row>
         <v-col cols="12">
-          <v-text-field
-            label="用户名/邮箱"
-            hide-details="auto"
-            type="text"
-            v-model="user"
-            required
-            color="primary"
-          ></v-text-field>
+          <v-text-field label="用户名/邮箱" hide-details="auto" type="text" v-model="user" required color="primary">
+          </v-text-field>
         </v-col>
       </v-row>
       <v-row>
         <v-col cols="12">
-          <v-text-field
-            label="密码"
-            hide-details="auto"
-            type="password"
-            v-model="password"
-            required
-          ></v-text-field>
+          <v-text-field label="密码" hide-details="auto" type="password" v-model="password" required></v-text-field>
         </v-col>
       </v-row>
       <v-row>
@@ -40,6 +28,7 @@
 
 <script>
 import AxiosInstance from '@/plugins/axios';
+import { load } from 'protobufjs';
 export default {
   data() {
     return {
@@ -51,13 +40,60 @@ export default {
 
   methods: {
     login() {
-      AxiosInstance({
-        url: '/hello',
-        method: 'get',
-      }).then(res => {
-        console.log(res);
-        sessionStorage.setItem('user', this.user);
-        this.$router.push('/');
+      load('user_login.proto', (err, root) => {
+        if(err) {
+          console.error(err);
+          return;
+        }
+        const UserInfoRequest = root.lookupType('user.UserInfoRequest');
+        const UserInfoResponse = root.lookupType('user.UserInfoResponse');
+
+        const payload = {
+          userId: 1
+        }
+
+        const errMsg = UserInfoRequest.verify(payload);
+        if (errMsg) {
+          console.error(errMsg);
+          return;
+        }
+
+        // Create a new message
+        var message = UserInfoRequest.create(payload); // or use .fromObject if conversion is necessary
+
+        // Encode a message to an Uint8Array (browser) or Buffer (node)
+        var buffer = UserInfoRequest.encode(message).finish();
+
+        var message1 = UserInfoRequest.decode(buffer);
+        var object = UserInfoRequest.toObject(message1, {
+            longs: String,
+            enums: String,
+            bytes: String,
+            // see ConversionOptions
+        });
+        console.log(object)
+
+        AxiosInstance({
+          url: '/hello',
+          method: 'post',
+          responseType: 'arraybuffer',
+          headers: {
+            'Content-Type': "application/x-protobuf"
+          },
+          data: buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength)
+        }).then(res => {
+          console.log(res);
+          var message = UserInfoResponse.decode(new Uint8Array(res));
+          var object = UserInfoResponse.toObject(message, {
+              longs: String,
+              enums: String,
+              bytes: String,
+              // see ConversionOptions
+          });
+          console.log(object)
+          sessionStorage.setItem('user', this.user);
+          this.$router.push('/');
+        })
       })
     },
     logout() {
